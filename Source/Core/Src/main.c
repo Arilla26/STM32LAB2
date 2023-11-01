06cc3337713df6dc635af544b67cec9c64f440e2
Arilla26 <146905469+Arilla26@users.noreply.github.com>
2023-11-01 22:39:56 +0700

    Init Ex8

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -53,7 +53,33 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int timer1_counter = 0;
+int timer1_flag = 0;
 
+void setTimer1(int duration)
+{
+	timer1_counter = duration/TIMER_CYCLE;
+	timer1_flag = 0;
+}
+
+int timer2_counter = 0;
+int timer2_flag = 0;
+
+void setTimer2(int duration)
+{
+	timer2_counter = duration/TIMER_CYCLE;
+	timer2_flag = 0;
+}
+
+void timer_run()
+{
+	if(timer0_counter > 0) timer0_counter--;
+	if(timer0_counter <= 0) timer0_flag = 1;
+	if(timer1_counter > 0) timer1_counter--;
+	if(timer1_counter <= 0) timer1_flag = 1;
+	if(timer2_counter > 0) timer2_counter--;
+	if(timer2_counter <= 0) timer2_flag = 1;
+}
 /* USER CODE END 0 */
 
 /**
