b5ce84cdd44c3d928930c1faa33d6111514d643f
Arilla26 <146905469+Arilla26@users.noreply.github.com>
2023-11-01 21:08:33 +0700

    Init Ex2

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -159,6 +159,62 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 		}
 	}
 }
+
+//TODO Ex2
+int count = 50;
+int m = -1;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+	count--;
+	if(count <= 0)
+	{
+		count = 50;
+		HAL_GPIO_TogglePin(RED_GPIO_Port, RED_Pin);
+		m++;
+		switch(m)
+		{
+		case 0:
+		{
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
+			HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+			display7SEG(1);
+			break;
+		}
+		case 1:
+		{
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
+			display7SEG(2);
+			break;
+		}
+		case 2:
+		{
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, RESET);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, SET);
+			HAL_GPIO_TogglePin(DOT_GPIO_Port, DOT_Pin);
+			display7SEG(3);
+			break;
+		}
+		case 3:
+		{
+			HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
+			HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+			HAL_GPIO_WritePin(EN2_GPIO_Port, EN2_Pin, SET);
+			HAL_GPIO_WritePin(EN3_GPIO_Port, EN3_Pin, RESET);
+			display7SEG(0);
+			m = -1;
+			break;
+		}
+		}
+	}
+}
 /* USER CODE END 4 */
 
 /**
